53140fd6c44c7d8215bf4e776ec29804c27463cc
Vivek Kumar <ervivekkumar677@gmail.com>
2024-05-11 18:34:53 +0530

    Folder sturcture Created

---
+++ expense-tracker-app/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import ExpenceTracker from './components/ExpenceTracker/ExpenceTracker';
+
+function App() {
+  return (
+    <ExpenceTracker />
+  );
+}
+
+export default App;
---
+++ expense-tracker-app/src/components/ExpenceTracker/ExpenceTracker.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ExpenceTracker = () => {
+
+    return (
+        <div>
+            <h1>Expence Tracker</h1>
+        </div>
+    );
+};
+
+export default ExpenceTracker;
